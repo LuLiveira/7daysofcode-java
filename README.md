ca1db8c3f2708abd2f53db3dfbe28ae4e00e2038
Lucas Silva <lcsd.lucas@gmail.com>
2022-03-31 22:37:55 -0300

    Primeiro dia

---
+++ README.md
@@ -0,0 +1,29 @@
+# #7DaysOfCode
+
+[7daysofcode](https://7daysofcode.io)
+
+São 7 exercícios práticos, enviados por e-mail, com as seguintes características:
+
+- Dificuldade crescente de desafios;
+- Feedback da comunidade;
+- O hábito de praticar.
+
+Como funciona?
+
+1) Você entra no site do 7 Days Of Code;
+
+2) Escolhe uma das trilhas. São 4 hoje (Java, React, HTML/CSS e lógica com JavaScript)
+
+3) Recebe desafio a cada dia, bem cedinho
+
+<hr>
+
+### Desafio 1
+
+* Fazer o seu código Java rodar e consumir a API do IMDB! Seu objetivo será imprimir os resultados de uma busca na linha de comando.<br>
+Usar essa API para pesquisar os top 250 filmes e imprimir o JSON correspondente no console da sua IDE.
+
+### Solução
+
+* Utilizar a API *HTTP* do **Java** para realizar a chamada para o **IMDB**.
+Utilizar variaveis de ambiente para não expor a *API_KEY* e exibir o resultado no console com *System.out.println*.
--- src/main/java/dev/lucas/App.java
+++ src/main/java/dev/lucas/App.java
@@ -1,7 +1,40 @@
 package dev.lucas;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.logging.Logger;
+
+import static java.lang.String.format;
+import static java.net.http.HttpClient.Version.HTTP_1_1;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    final static Logger log = Logger.getAnonymousLogger();
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final var apiKey = System.getenv("API_KEY");
+        log.info(format("API_KEY=%s", apiKey));
+
+        final var imdbUrl = format("https://imdb-api.com/en/API/Top250Movies/%s", apiKey);
+        log.info(format("IMDB_URL=%s", imdbUrl));
+
+        final var uri = URI.create(imdbUrl);
+
+        final var request = HttpRequest
+                .newBuilder(uri)
+                .GET()
+                //.timeout(Duration.ofSeconds(1)) //opcional
+                .version(HTTP_1_1) //opcional
+                .build();
+
+        final var client = HttpClient.newBuilder().build();
+
+        final var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        log.info(format("RESPONSE_STATUS_CODE=%s", response.statusCode()));
+
+        System.out.println(response.body());
     }
 }
